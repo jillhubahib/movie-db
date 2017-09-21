322b85c5eb4cd0fbee5db05857533f8d3f9e3323
Jill Hubahib <jillhubahib@gmail.com>
2017-09-21 14:46:33 +0800

    Show individiual Movie in React

--- app/javascript/movies/components/MovieItem.jsx
+++ app/javascript/movies/components/MovieItem.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 export const MovieImage = ({ name, src }) => (
   <img alt={name} src={src} />
@@ -10,23 +10,26 @@ export const VoteBadge = ({ vote_average }) => (
   </h6>
 )
 
-const imageSize = 'w300_and_h450_bestv2'
+class MovieItem extends Component {
+  render() {
+    const { title, imagePath, poster_path, vote_average } = this.props
+    const imageSize = 'w300_and_h450_bestv2'
+    const src = poster_path
+                ? `${imagePath}${imageSize}${poster_path}`
+                : undefined
 
-const MovieItem = ({ title, imagePath, poster_path, vote_average }) => {
-  const src = poster_path
-              ? `${imagePath}${imageSize}${poster_path}`
-              : undefined
-  return (
-    <div className="card">
-      <a>
-        <MovieImage name={title}
-          src={src}
-        />
-      </a>
-      <p className="card-text"></p>
-      <VoteBadge vote_average={vote_average} />
-    </div>
-  )
+    return (
+      <div className="card">
+        <a>
+          <MovieImage name={title}
+            src={src}
+          />
+        </a>
+        <p className="card-text"></p>
+        <VoteBadge vote_average={vote_average} />
+      </div>
+    )
+  }
 }
 
 export default MovieItem
